00f8aee009142338194faa2bec951e98a79ee3e4
danbart909@gmail.com <danbart909@gmail.com>
2025-10-05 11:08:03 -0400

    added special wrkr pages

--- src/pages/DeleteAccount.js
+++ src/pages/DeleteAccount.js
@@ -26,6 +26,14 @@ export default function DeleteAccount() {
       >
         Here you can put in a request to have your account deleted from the wrkr app.
       </Row>
+      <Row
+        m='1vw'
+        p='1vw'
+        justifyContent='center'
+        border='1px solid white'
+      >
+        Form goes here
+      </Row>
     </Col>
   )
 }
